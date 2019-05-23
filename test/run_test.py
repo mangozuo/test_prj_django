fd3981fcd4d9ba2dffb80d5770eff7d9f2842dbb
Administrator <admin@example.com>
2019-05-23 20:47:28 +0800

    添加自动化测试脚本

---
+++ test/run_test.py
@@ -0,0 +1,17 @@
+#!usr/bin/python3
+# encoding: utf-8
+
+"""
+@author:dell
+@file: run_test.py
+@time: 2019/05/23
+"""
+import os
+import unittest
+
+if __name__ == '__main__':
+    case_path = os.path.join(os.getcwd(),"testCase")
+    print(case_path)
+    suite = unittest.defaultTestLoader.discover(case_path)
+
+    unittest.TextTestRunner().run(suite)
---
+++ test/testCase/test_login.py
@@ -0,0 +1,36 @@
+#!usr/bin/python3
+# encoding: utf-8
+
+"""
+@author:dell
+@file: test_login.py
+@time: 2019/05/23
+"""
+import unittest
+from selenium import webdriver
+import time
+import os
+
+class TestLogin(unittest.TestCase):
+    def setUp(self):
+        exe_path = os.path.join(os.getcwd(),"tools","chromedriver")
+        self.driver = webdriver.Chrome(executable_path=exe_path)
+        self.driver.get("http://127.0.0.1:8000/login/")
+
+    def test_login_1(self):
+        self.driver.find_element_by_name("username").send_keys("testops")
+        self.driver.find_element_by_name("password").send_keys("test123456")
+        self.driver.find_element_by_id("btn").click()
+        time.sleep(5)
+        actText = self.driver.find_element_by_tag_name("body").text
+        eptText = "Welcome to my WebSuite!"
+        self.assertEqual(actText,eptText,"登录不成功或者登录后显示错误")
+
+    def tearDown(self):
+        time.sleep(3)
+        self.driver.quit()
+
+if __name__ == '__main__':
+    unittest.main()
+
+
